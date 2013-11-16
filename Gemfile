fe751076a748f95a52e25c03772aacdc52705c8e
Sandeep Arneja <sandeep45@gmail.com>
2013-11-15 23:26:53 -0800

    added redis

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'unicorn'
 
 gem "rack-timeout"
 
+gem 'redis'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ CookieMonster::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
 end
+
+  ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
