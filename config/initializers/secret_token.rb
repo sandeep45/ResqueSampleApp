3e205de04a3597e42efb6f6e99c33758111e68ee
Sandeep Arneja <sandeep45@gmail.com>
2013-11-15 22:38:47 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookieMonster::Application.config.secret_token = 'dad775b0a13d06850fd6fd8105ff32fc70cdcf5af5d1596dca6e92fc7a8e914c59c5f1b009cb25f7938a5c2236de381995fd5816f0ee8c8be746b82e9977d1e5'
